55d3993c60d48dee6c5f7f44baac7936d9a00f1c
Brian Macdonald <brian.m17@gmail.com>
2009-09-15 18:33:38 -0400

    Consolidated database and model.

--- system/Starch/Database.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package StarchMVC
- * @author Brian Macdonald <brian@zycot.com>
- * @copyright Copyright (c) 2009, Brian Macdonald
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License  
- */
-
-require_once BASE_DIR.'/system/Starch/Database/outlet_wrapper.php'; 
--- system/Starch/Model.php
+++ system/Starch/Model.php
@@ -4,20 +4,6 @@
  * @author Brian Macdonald <brian@zycot.com>
  * @copyright Copyright (c) 2009, Brian Macdonald
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License  
- */ 
+ */
 
-
-class Starch_Model
-{
-
-    /**
-     * Builds the model
-     */
-    public function __construct()
-    {
-    
-    }
-
-
-
-} 
+require_once BASE_DIR.'/system/Starch/Database/outlet_wrapper.php';        
